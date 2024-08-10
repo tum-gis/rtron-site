60dbdbbe051dcb88b575c41080ae13f389129965
Benedikt Schwab <benedikt.schwab@gmail.com>
2024-08-10 19:24:20 +0200

    fixed paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     mode: "jit",
     content: [
-        "./src/components/**/*.js",
-        "./src/pages/**/*.{md,mdx}",
-        "./src/theme.config.tsx",
+        "./components/**/*.js",
+        "./pages/**/*.{md,mdx}",
+        "./theme.config.tsx",
     ],
 };
